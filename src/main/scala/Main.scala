12d073481da0832f85afd2fa78e8be03c03c096e
Manuel Amet <emmanuelamet11@gmail.com>
2022-11-16 06:02:41 +0000

    code updated

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,8 @@
 import java.util.Date
+import java.util.concurrent.Future
+import scala.concurrent.Future
+import scala.concurrent.Future.never.recover
+import scala.util.Try
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -68,3 +72,109 @@ object Main {
       return f"$number is an odd number"
   }
 }
+
+object Application extends App{
+  //Map
+  val nameMap = Map("fName" -> "Emmanuel", "lName" -> "Amet", "age" -> 110)
+
+  //FlatMap
+  val names = Seq("Emmanuel", "Amet")
+  val lNames = Seq("Amet")
+  println(names.flatMap(_.toUpperCase()))
+
+  //Filter
+  val numbers = List(1,2,3,4,5,6,7,8)
+  val filter = numbers.filter(_ > 5)
+  println(filter)
+
+  //Partition
+  val list1 = List(1,2,3,4,5,6)
+  val part1 = list1.partition(_ > 2)
+  println(part1)
+
+  //Zip
+  val list2 = List(1,2,3,4,5)
+  val list3 = List(6,7,8,9,0)
+  val zip = list2.zip(list2)
+  println(zip)
+
+  //Foreach
+  val list4 = List(1,2,3,4,5,6)
+  list4 foreach(println)
+
+  for (x <- list4) yield println(x + 2)
+
+  //Options
+  def partn(str: Option[String]) = str match {
+    case Some(s) => s
+    case None => ("Key not found")
+  }
+
+  val days = Map("one" -> "Monday", "two" -> "Tuesday")
+  println(partn(days.get("one")))
+
+  val countries = List("Ghana", "UK", "USA")
+  val capitals = Map("Ghana" -> "Accra", "UK" -> "London", "USA" -> "Washington DC")
+  val flat = countries flatMap(c => capitals.get(c))
+  val token: Option[String] = None
+  println(flat)
+
+  for(c <- countries; cap <- capitals.get(c)) yield println((c, cap))
+
+  def computeDivision(a:Int, b:Int): Either[String, Double] = {
+    if(a == 0) return Left("Division impossible")
+    Right(a / b)
+  }
+
+  def computeDivision2(a: Int, b:Int): Double = {
+    a / b
+  }
+
+  println(computeDivision(9, 4))
+
+  //Try catch
+  try{
+    println(computeDivision2(99, 0))
+  }catch {
+    case e: ArithmeticException => println("Arithmetic Exception")
+    case _ => println("Error")
+  }
+
+  val num = "111"
+//  Try (num.toInt)
+//  recover{
+//    case e => "Error"
+//  }
+
+
+  def passedByValue(a: Int) = a *a
+  def passedByName(a: => Int) = a * a
+
+  println(passedByName(2))
+  println(passedByValue(2))
+
+  //Curried
+  def add(a: Int)(b:Int): Int = {
+    a * b
+  }
+
+  println(add(2)(5 + 4))
+
+  //Partially define func
+  val pf: PartialFunction[Int, String] = {
+    case 1 => "One"
+    case 2 => "Two"
+    case 3 => "Three"
+  }
+
+  println(pf isDefinedAt(5))
+  println(pf(2))
+}
+
+object SquareRoot extends PartialFunction[Int, Double] {
+  override def isDefinedAt(x: Int) = (x >= 1)
+
+  override def apply(v1: Int) = if(v1 >= 0) Math.sqrt(v1) else
+    throw new IllegalArgumentException("")
+}
+
